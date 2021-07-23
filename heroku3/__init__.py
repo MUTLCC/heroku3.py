2e8ed5aaa4b8a0c5460b326fd684831f796727fa
MUTLU <65467091+MUTLCC@users.noreply.github.com>
2021-07-23 13:50:06 +0300

    Changed name

--- heroku3/__init__.py
+++ heroku3/__init__.py
@@ -6,7 +6,7 @@
 
 
 """
-heroku3.py
+herokum
 ~~~~~~~~~
 
 :copyright: (c) 2013 by Heroku.
@@ -15,12 +15,12 @@ heroku3.py
 
 # Meta.
 
-__title__ = "heroku3"
+__title__ = "herokum"
 __author__ = "Heroku"
 __copyright__ = "Copyright 2013 Heroku"
 __license__ = "MIT"
 
-__version__ = "4.2.5"
+__version__ = "4.2.6"
 
 # Module namespace.
 
